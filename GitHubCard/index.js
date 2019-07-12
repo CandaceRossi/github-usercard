391927c2d50f8878ccc6f3d110dd8c676fff4808
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-07-12 10:49:13 -0400

    pulled the api info through to the card, just working on style part

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,14 +2,21 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cards = document.querySelector('.cards')
+console.log("woopee", cards)
+
 axios.get('https://api.github.com/users/CandaceRossi')
     .then((data) => {
         //here is where we get results from server
-
         console.log('response', data)
+        let card = data.data
+        console.log("woohoo", card)
+        const element = createGithubCard(card)
+        console.log('yeehaw', element)
+        cards.appendChild(element)
     })
     .catch((error) => {
-        console.log('ERROR');
+        console.log('ERROR', error);
     })
     /* Step 2: Inspect and study the data coming back, this is YOUR 
        github info! You will need to understand the structure of this 
@@ -23,9 +30,6 @@ axios.get('https://api.github.com/users/CandaceRossi')
            create a new component and add it to the DOM as a child of .cards
 */
 
-const cards = querySelector('.cards')
-    // const candace = createGithubCard('https://api.github.com/users/candacerossi', 'Candace Rossi')
-    // cards.appendChild('candace')
 
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
@@ -38,78 +42,99 @@ const cards = querySelector('.cards')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-followersArray.forEach(data => {
+// const followersArray = [
+//     axios.get(`https://api.github.com/users/tetondan`),
+//     axios.get(`https://api.github.com/users/dustinmyers`),
+//     axios.get(`https://api.github.com/users/justsml`),
+//     axios.get(`https://api.github.com/users/luishrd`),
+//     axios.get(`https://api.github.com/users/bigknell`),
+//     axios.get(`https://api.github.com/users/danbergelt`)
+// ];
+// followersArray.forEach(person => {
+//         user.then((data) => {
+//                 //here is where we get results from server
+//                 console.log('response', data)
+//                 let card = data.data
+//                 console.log("woohoo", card)
+//                 const element = createGithubCard(card)
+//                 console.log('yeehaw', element)
+//                 cards.appendChild(element)
+//             })
+//             .catch((error) => {
+//                 console.log('ERROR');
+//             })
+//     })
+/* Step 3: Create a function that accepts a single object as its only argument,
+          Using DOM methods and properties, create a component that will return the following DOM element:
 
-    })
-    /* Step 3: Create a function that accepts a single object as its only argument,
-              Using DOM methods and properties, create a component that will return the following DOM element:
+<div class="card">
+  <img src={image url of user} />
+  <div class="card-info">
+    <h3 class="name">{users name}</h3>
+    <p class="username">{users user name}</p>
+    <p>Location: {users location}</p>
+    <p>Profile:  
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+    <p>Followers: {users followers count}</p>
+    <p>Following: {users following count}</p>
+    <p>Bio: {users bio}</p>
+  </div>
+</div>
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:  
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-
-    */
+*/
 
 function createGithubCard(userObject) {
+
     //create elements
     const userCard = document.createElement('div')
     const userImg = document.createElement('img')
     const cardInfo = document.createElement('div')
     const govName = document.createElement('h3')
-    const username = document.createElement('p')
+    const userName = document.createElement('p')
     const userLocation = document.createElement('p')
     const userProfile = document.createElement('p')
     const userGitPage = document.createElement('a')
     const userFollowers = document.createElement('p')
     const userFollowing = document.createElement('p')
-
+    const userBio = document.createElement('p')
 
     //set styles
-    userCard.classList.add(userCard)
-    cardInfo.classList.add(userImg)
-    cardInfo.classList.add(govName)
-    cardInfo.classList.add(userName)
-    cardInfo.classList.add(userLocation)
-    cardInfo.classList.add(userProfile)
-    cardInfo.classList.add(userGitPage)
-    cardInfo.classList.add(userFollowers)
-    cardInfo.classList.add(userFollowing)
+    userCard.classList.add('card')
+    userImg.classList.add('img')
+    cardInfo.classList.add('card-info')
+    govName.classList.add('name')
+    userName.classList.add('username')
+    userLocation.classList.add('card-p')
+    userProfile.classList.add('card-p')
+    userFollowers.classList.add('card-p')
+    userFollowing.classList.add('card-p')
+    userBio.classList.add('card-p')
+
 
     //set the content
-    userImg.src = `${}`
-    govName.textContent = `${userObject.}`
-    userName.textContent = `${userObject.}`
+    userImg.src = `${userObject.avatar_url}`
+    govName.textContent = `${userObject.login}`
+    userName.textContent = `${userObject.name}`
     userLocation.textContent = `${userObject.location}`
-    userProfile.textContent = `${userObject.}`
-    userGitPage.textContent = `${userObject.}`
-    userFollowers.textContent = `${userObject.}`
-    userFollowing.textContent = `${userObject.}`
+    userProfile.textContent = `Profile ${userObject.url}`
+    userGitPage.textContent = `Git Page ${userObject.repos_url}`
+    userFollowers.textContent = `followers ${userObject.followers}`
+    userFollowing.textContent = `following ${userObject.following}`
 
     //put together
+    userCard.appendChild(cardInfo)
     userCard.appendChild(userImg)
-    userCard.appendChild(govName)
-    userCard.appendChild(userName)
-    userCard.appendChild(userLocation)
-    userCard.appendChild(userProfile)
-    userCard.appendChild(userGitPage)
-    userCard.appendChild(userFollowers)
-    userCard.appendChild(userFollowing)
+    cardInfo.appendChild(govName)
+    cardInfo.appendChild(userName)
+    cardInfo.appendChild(userLocation)
+    cardInfo.appendChild(userProfile)
+    cardInfo.appendChild(userFollowers)
+    cardInfo.appendChild(userFollowing)
+    userProfile.appendChild(userGitPage)
 
 
-
-    return card
+    return userCard
 }
 
 

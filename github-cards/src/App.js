2b31104a8b2ff19e8ac1f3d513d53935e48ddcba
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-09-03 16:59:21 -0400

    was able to pull through my data, set my state to response and make my card input dynamic data with this.state

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -3,27 +3,41 @@ import "./App.css";
 import axios from "axios";
 
 class App extends React.Component {
-  state = {
-    gitCard: [],
-    gitCardText: ""
-  };
-
-  componentDidMount() {
-    // axios
-    // .get('https://api.github.com/users/CandaceRossi')
-    // .then(res => res.json())
-    // .then(jsonData => this.ListeningStateChangedEvent({gitCard.message}))
-    // .catch(err => console.log("Error"))
+  constructor() {
+    super();
+    this.state = {
+      gitCard: []
+    };
   }
 
-  componentDidUpdate() {}
+  componentDidMount() {
+    axios.get("https://api.github.com/users/CandaceRossi").then(res => {
+      console.log("is there a response here?", res);
+      this.setState(res.data);
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("cdu is running");
+  }
   render() {
     return (
       <div className="container">
         <h1 className Header>
           GitHub Cards
         </h1>
-        <div className="cards">GitHub Cards</div>
+        <div class="card">
+          <img src={this.state.image} />
+          <div class="card-info">
+            <h3 class="name">{this.state.name}</h3>
+            <p class="username">{this.state.username}</p>
+            <p>Location: {this.state.location}</p>
+            <p>Profile:</p>
+            <p>Followers: {this.state.followers}</p>
+            <p>Following: {this.state.following}</p>
+            <p>Bio: {this.state.bio}</p>
+          </div>
+        </div>
       </div>
     );
   }

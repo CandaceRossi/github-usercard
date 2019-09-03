1760bdaea20eaa223bbe12207be8fb859e252326
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-09-03 18:47:37 -0400

    working on styling the card

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -2,6 +2,28 @@ import React from "react";
 import "./App.css";
 import axios from "axios";
 
+// const followersArray = [
+//     axios.get(`https://api.github.com/users/tetondan`),
+//     axios.get(`https://api.github.com/users/dustinmyers`),
+//     axios.get(`https://api.github.com/users/justsml`),
+//     axios.get(`https://api.github.com/users/luishrd`),
+//     axios.get(`https://api.github.com/users/bigknell`),
+//     axios.get(`https://api.github.com/users/danbergelt`)
+// ];
+// followersArray.forEach(person => {
+//         .then((data) => {
+//                 //here is where we get results from server
+//                 // console.log('response', data)
+//                 // let card = data.data
+//                 // console.log("woohoo", card)
+//                 // const element = createGithubCard(card)
+//                 // console.log('yeehaw', element)
+//                 // cards.appendChild(element)
+//             })
+//             .catch((error) => {
+//                 console.log('ERROR');
+//             })
+//     })
 class App extends React.Component {
   constructor() {
     super();
@@ -22,20 +44,18 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div className="container">
-        <h1 className Header>
-          GitHub Cards
-        </h1>
-        <div class="card">
-          <img src={this.state.image} />
-          <div class="card-info">
-            <h3 class="name">{this.state.name}</h3>
-            <p class="username">{this.state.username}</p>
-            <p>Location: {this.state.location}</p>
-            <p>Profile:</p>
+      <div className="App">
+        <h1 className="App-header">GitHub Cards</h1>
+        <div class="cards">
+          <div class="card">
+            <img className="card img" src={this.state.avatar_url} />
+            <h3 className="name">Name: {this.state.login}</h3>
+            <p className="username">UserName: {this.state.login}</p>
+            <p>Created At: {this.state.created_at}</p>
+            <p>Updated At: {this.state.updated_at}</p>
+            <p>Git URL: {this.state.url}</p>
             <p>Followers: {this.state.followers}</p>
             <p>Following: {this.state.following}</p>
-            <p>Bio: {this.state.bio}</p>
           </div>
         </div>
       </div>
